64bdd7a2f1117c404a76b30019f5585ae68073b8
Kyle Klintworth <klintyk@byu.edu>
2023-04-09 16:54:05 -0600

    init

---
+++ backend/Mission14Api/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14Api.Data;
+
+namespace Mission14Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private MovieDbContext Context { get; set; }
+        public MoviesController(MovieDbContext temp)
+        {
+            Context = temp;
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            var x = Context.Movies!
+                .Where(x => x.Edited == "Yes")
+                .OrderBy(x => x.Title)
+                .ToArray();
+            return x;
+        }
+    }
+}
---
+++ backend/Mission14Api/Data/MovieDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Mission14Api.Data
+{
+    public partial class MovieDbContext : DbContext
+    {
+        public MovieDbContext()
+        {
+        }
+
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Name=ConnectionStrings:MovieDbConn");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.HasNoKey();
+            });
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
